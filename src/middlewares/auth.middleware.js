1ad5a1808d0072da83e7134e534ad78d7c1c21dc
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-05-01 20:55:40 +0530

    small refactor

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -16,7 +16,7 @@ export const protectRoute = (req, res, next) => {
         .status(401)
         .json({ message: "Unauthorized - Invalid token!" });
 
-    const user = await User.findById(decoded.userId);
+    const user = await User.findById(decoded.userId).select("-password");
     if (!user)      return res
         .status(401)
         .json({ message: "Unauthorized - User not found!" });
