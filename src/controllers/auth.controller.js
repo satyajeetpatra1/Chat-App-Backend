13e27507b94b68cecc799742d343e3a081cb2b83
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-05-01 20:01:34 +0530

    adding login logout functionality

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -62,3 +62,43 @@ export const signup = async (req, res) => {
       .json({ message: `Internal Server Error! : ${error}` });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid Credentials!" });
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in Login Controller: ", error);
+    return res
+      .status(500)
+      .json({ message: `Internal Server Error! : ${error}` });
+  }
+};
+
+export const logout = async (_, res) => {
+  try {
+    res.cookie("jwt", "", {maxAge: 0});
+    res.status(200).json({message: "Logout successful!"})
+  } catch (error) {
+    console.log("Error in Logout Controller: ", error);
+    return res
+      .status(500)
+      .json({ message: `Internal Server Error! : ${error}` });
+  }
+};
