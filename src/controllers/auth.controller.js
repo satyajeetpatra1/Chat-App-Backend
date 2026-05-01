0808d7fccb44c70b7a3741034384767aad51b8ef
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-05-01 20:12:31 +0530

    small refactor

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -66,6 +66,9 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
   const { email, password } = req.body;
 
+  if (!email || !password)
+    return res.status(400).json({ error: "Email and Password are required" });
+
   try {
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
@@ -93,8 +96,8 @@ export const login = async (req, res) => {
 
 export const logout = async (_, res) => {
   try {
-    res.cookie("jwt", "", {maxAge: 0});
-    res.status(200).json({message: "Logout successful!"})
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logout successful!" });
   } catch (error) {
     console.log("Error in Logout Controller: ", error);
     return res
