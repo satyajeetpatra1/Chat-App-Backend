b4c1a80a35bfa1e61b8050b9e6bf274e30a290fa
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-05-23 21:13:35 +0530

    small bug fix

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -16,7 +16,7 @@ export const protectRoute = async (req, res, next) => {
         .status(401)
         .json({ message: "Unauthorized - Invalid token!" });
 
-    const user = await User.findById(decoded.userId).select("-password");z
+    const user = await User.findById(decoded.userId).select("-password");
     if (!user)      return res
         .status(401)
         .json({ message: "Unauthorized - User not found!" });
